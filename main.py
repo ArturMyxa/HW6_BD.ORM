b72482189fb018743c5cdeb963e8c897ff56e01d
Artur Ruzilevich <bigflyfly33@gmail.com>
2025-01-22 13:28:54 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sqlalchemy
 from sqlalchemy.orm import declarative_base, relationship, sessionmaker
-from models import Publisher, Book, Shop, Stock, Sale 
- 
+from models import Publisher, Book, Shop, Stock, Sale
 
 Base = declarative_base()
 
@@ -12,28 +11,24 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 
+def get_shops(publisher_input):
+    query = session.query(
+        Shop, Stock, Book, Publisher, Sale
+    ).select_from(Shop).join(Stock).join(Book).join(Publisher).join(Sale)
 
-def searching_publisher_name():
-    query_join = session.query(Shop).join(Stock).join(Book).join(Publisher)
-    query_publisher_name = input('Введите имя (name) издателя: ')
-    query_result = query_join.filter(Publisher.publisher_name == query_publisher_name)
-    for result in query_result.all():
-        print(f'Издатель "{query_publisher_name}" найден в магазине "{result.name}" с идентификатором {result.id}')
+    if publisher_input.isdigit():
+        query = query.filter(Publisher.id_publisher == int(publisher_input))
+    else:
+        query = query.filter(Publisher.publisher_name == publisher_input)
 
+    results = query.all()
 
-def searching_publisher_id():
-    query_join = session.query(Shop).join(Stock).join(Book).join(Publisher)
-    query_publisher_name = input('Введите идентификатор (id) издателя: ')
-    query_result = query_join.filter(Publisher.id_publisher == query_publisher_name)
-    for result in query_result.all():
-        print(
-            f'Издатель c id: {query_publisher_name} найден в магазине "{result.name}" '
-            f'с идентификатором {result.id}')
+    for shop, stock, book, publisher, sale in results:
+        print(f"{book.name: <40} | {shop.name: <10} | {sale.price: <8} | {sale.date:%d-%m-%Y}")
 
 
 if __name__ == '__main__':
-    searching_publisher_name()
-    searching_publisher_id()
+    publisher_input = input("Введите имя или идентификатор издателя: ")
+    get_shops(publisher_input)
 
-
-session.close()
+    session.close()
